24d76f0badf30a24e6658ce208bce0880e086435
Michael Butak <mikebutak@gmail.com>
2018-06-18 17:36:11 -0700

    config migrations and seed

---
+++ database/db.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const config = require('../knexfile.js');
+const env = 'development';
+const knex = require('knex')(config[env]);
+
+module.exports = knex;
--- database/queries.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const config = require('../knexfile.js');
-const env = 'development';
-const knex = require('knex')(config[env]);
-
-module.exports = knex;
